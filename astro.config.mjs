027fa7024305b4970354a9bd0f73d0bdece036e4
m0dh4x <info@florian-drews.de>
2024-05-06 16:33:32 +0200

    build: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  integrations: [react(), tailwind()]
+  output: "hybrid",
+  integrations: [react(), tailwind()],
+  adapter: vercel()
 });
