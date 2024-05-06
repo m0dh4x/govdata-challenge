c7df7fde34859553623ae51057f95c72f287982d
m0dh4x <info@florian-drews.de>
2024-05-06 12:01:42 +0200

    feat(dashboard): add sortable cards component and change card from astro to react component

---
+++ src/components/Card.tsx
@@ -0,0 +1,29 @@
+import { BarChart2 } from "lucide-astro";
+import type { GovData } from "types/types";
+
+export const Card = ({ department, datasets }: GovData) => {
+  return (
+    <div className="rounded-lg border bg-card text-card-foreground shadow-sm p-4">
+      <div className="flex justify-between">
+        <h3 className="text-balance	">{department}</h3>
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            className="text-2xl"
+          >
+            <line x1="18" x2="18" y1="20" y2="10" />
+            <line x1="12" x2="12" y1="20" y2="4" />
+            <line x1="6" x2="6" y1="20" y2="14" />
+          </svg>
+        </div>
+      </div>
+      <p className="mt-6 text-2xl font-bold">{datasets}</p>
+      <p className="text-xs text-gray-500">Datasets</p>
+    </div>
+  );
+};
---
+++ src/components/SortableCards.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import type { GovData } from "types/types";
+import { sortData } from "@/utils/sortData";
+import { Card } from "./Card";
+
+type SortableCardsProps = {
+  govData: GovData[];
+};
+
+export const SortableCards = ({ govData }: CardsProps) => {
+  const govDataCopy = [...govData];
+  const [order, setOrder] = useState<"asc" | "desc">("asc");
+  const [list, setList] = useState(govData);
+
+  React.useEffect(() => {
+    const sortedList = sortData(govDataCopy, order);
+    setList(sortedList);
+  }, [govData, order]);
+
+
+  return (
+    <>
+      <select
+        defaultValue={"asc"}
+        onChange={(e) => setOrder(e.target.value as "asc" | "desc")}
+      >
+        <option value="asc">Aufsteigend</option>
+        <option value="desc">Absteigend</option>
+      </select>
+      {list.map((item) => {
+        return <Card key={`${item.department}-${item.datasets}`} {...item} />;
+      })}
+    </>
+  );
+};
